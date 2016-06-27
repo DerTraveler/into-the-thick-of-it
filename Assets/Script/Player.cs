7867c37a722194e2fd49a8ede2d180641b10fd1b
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-06-28 08:43:43 +0900

    Basic 4 direction turning.

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Animator animator;
+	private SpriteRenderer spriteRenderer;
+
+	private int direction;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw("Horizontal") != 0) {
+			direction = (int) Input.GetAxisRaw("Horizontal") * 2;
+			spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") < 0;
+		} else if (Input.GetAxisRaw("Vertical") != 0) {
+			direction = (int) Input.GetAxisRaw("Vertical");
+		}
+		animator.SetInteger("Direction", direction);
+	}
+}
