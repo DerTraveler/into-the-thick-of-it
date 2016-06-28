0357e4ac0e35766d937c74f01986cb1ba7da0305
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-06-28 09:41:37 +0900

    Switch to manual animation change. 4 Direction Walk.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -12,7 +12,16 @@ public class Player : MonoBehaviour {
 	private Animator animator;
 	private SpriteRenderer spriteRenderer;
 
-	private int direction;
+	public enum State {
+		Idle,
+		Walking,
+		Attacking
+	}
+
+	public State state = State.Idle;
+
+	public int direction = -1;
+	public bool directionPressed = false;
 
 	void Start () {
 		animator = GetComponent<Animator>();
@@ -21,12 +30,60 @@ public class Player : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		ReadInput();
+
+		UpdateState();
+	}
+
+	private void ReadInput() {
 		if (Input.GetAxisRaw("Horizontal") != 0) {
 			direction = (int) Input.GetAxisRaw("Horizontal") * 2;
-			spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") < 0;
+			directionPressed = true;
 		} else if (Input.GetAxisRaw("Vertical") != 0) {
 			direction = (int) Input.GetAxisRaw("Vertical");
+			directionPressed = true;
+		} else {
+			directionPressed = false;
 		}
-		animator.SetInteger("Direction", direction);
+	}
+
+	private void UpdateState() {
+		switch(state) {
+			case State.Idle:
+				if (directionPressed) {
+					state = State.Walking;
+				}
+				break;
+			case State.Walking:
+				if (!directionPressed) {
+					state = State.Idle;
+				}
+				break;
+		}
+
+		PlayAnimation();
+	}
+
+	private void PlayAnimation() {
+		spriteRenderer.flipX = direction == -2;
+		switch (state) {
+			case State.Idle:
+				animator.Play("Idle" + getDirectionName(direction));
+				break;
+			case State.Walking:
+				animator.Play("Walk" + getDirectionName(direction));
+				break;
+		}
+	}
+
+	private string getDirectionName(int direction) {
+		switch (direction) {
+		case 1:
+			return "Up";
+		case -1:
+			return "Down";
+		default:
+			return "Side";
+		}
 	}
 }
