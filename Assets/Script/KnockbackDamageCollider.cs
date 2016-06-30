bdcb677eae82d9e29d9f3525e964e8c714d02690
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-06-30 09:44:36 +0900

    Change normal collisions to trigger.
    To avoid physical reactions (actio-reactio) between the during weapon attacks.

--- Assets/Script/KnockbackDamageCollider.cs
+++ Assets/Script/KnockbackDamageCollider.cs
@@ -12,17 +12,17 @@ public class KnockbackDamageCollider : MonoBehaviour {
 	public int attackDamage = 1;
 	public float knockbackForce = 1.0f;
 
-	void OnCollisionEnter2D(Collision2D coll) {
-		WorldObject target = coll.gameObject.GetComponent<WorldObject>();
+	void OnTriggerEnter2D(Collider2D other) {
+		WorldObject target = other.attachedRigidbody.GetComponent<WorldObject>();
 
 		if (target.ReceiveDamage(attackDamage)) {
-			Vector2 hitForce = coll.contacts[0].point - (Vector2)transform.position;
+			Vector2 hitForce = other.bounds.center - transform.position;
 			if (hitForce.x > hitForce.y) {
 				hitForce.Set(Mathf.Sign(hitForce.x), 0);
 			} else {
 				hitForce.Set(0, Mathf.Sign(hitForce.y));
 			}
-			coll.rigidbody.AddForce(hitForce.normalized * knockbackForce, ForceMode2D.Impulse);
+			other.attachedRigidbody.AddForce(hitForce.normalized * knockbackForce, ForceMode2D.Impulse);
 		}
 	}
 
