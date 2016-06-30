8f8b452c00c22f28bff01adddf7bb896ecd5db94
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-06-30 11:14:54 +0900

    Extract modular class for Pixel perfect positioning.

--- Assets/Script/WorldObject.cs
+++ Assets/Script/WorldObject.cs
@@ -12,25 +12,16 @@ public abstract class WorldObject : MonoBehaviour {
 	public GameObject body;
 	private SpriteRenderer rend;
 
-	private float unitPerPixel;
-
 	void Awake () {
 		rend = body.GetComponent<SpriteRenderer>();
-
-		unitPerPixel = 1.0f / rend.sprite.pixelsPerUnit;
+		gameObject.AddComponent<PixelPerfectPositioner>();
 	}
 
 	void LateUpdate () {
 		if (rend.isVisible) {
 			// Dynamic draw order based on y-coordinate
 			rend.sortingOrder = (int) (body.transform.position.y * 64.0 * -1.0f);
-			// Round position to next pixel perfect position
-			transform.position = RoundToPixelPerfect(transform.position);
 		}
-	}
-
-	private Vector2 RoundToPixelPerfect(Vector2 pos) {
-		return new Vector2(Mathf.Round(pos.x / unitPerPixel) * unitPerPixel, Mathf.Round(pos.y / unitPerPixel) * unitPerPixel);
 	}
 
 	public Spawner spawnedBy = null;
