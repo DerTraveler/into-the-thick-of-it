f441abc72b7079f13316c827ec4415fe424308d7
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-06-30 21:45:07 +0900

    Add multiple Sound Channels

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class AudioManager : MonoBehaviour {
 
-	public AudioSource sfxSource;
 	public AudioSource musicSource;
+	public AudioSource[] sfxSources;
 
 	public static AudioManager instance = null;
 
@@ -18,8 +18,22 @@ public class AudioManager : MonoBehaviour {
 	}
 
 	public void PlaySound(AudioClip clip) {
-		sfxSource.clip = clip;
-		sfxSource.Play();
+		print("Try to play sound: " + clip.name);
+		AudioSource freeSource = null;
+
+		foreach (AudioSource src in sfxSources) {
+			if (!src.isPlaying) {
+				freeSource = src;
+			}
+			if (src.clip == clip && src.isPlaying) {
+				return;
+			}
+		}
+		if (freeSource != null) {
+			print("Play sound: " + clip.name);
+			freeSource.clip = clip;
+			freeSource.Play();	
+		}
 	}
 
 }
