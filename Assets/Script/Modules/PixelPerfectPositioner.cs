6374ebf74193d6d05208cf510f9d5d3718c11744
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-06-30 11:14:54 +0900

    Extract modular class for Pixel perfect positioning.

---
+++ Assets/Script/Modules/PixelPerfectPositioner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Fixes position to an always pixel perfect position.
+/// </summary>
+public class PixelPerfectPositioner : MonoBehaviour {
+
+	private readonly float unitPerPixel = 1.0f / 64.0f;
+
+	void LateUpdate () {
+		// Round position to next pixel perfect position
+		transform.position = RoundToPixelPerfect(transform.position);
+	}
+
+	private Vector2 RoundToPixelPerfect(Vector2 pos) {
+		return new Vector2(Mathf.Round(pos.x / unitPerPixel) * unitPerPixel, Mathf.Round(pos.y / unitPerPixel) * unitPerPixel);
+	}
+
+}
