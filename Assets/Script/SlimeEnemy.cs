09bd06f40d595c7e533d746a74bc313c393041f9
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-06-30 23:03:44 +0900

    SlimeEnemy goes back to idle after Hurt.

--- Assets/Script/SlimeEnemy.cs
+++ Assets/Script/SlimeEnemy.cs
@@ -90,6 +90,11 @@ public class SlimeEnemy : WorldObject {
 				}
 			}
 			break;
+		case State.Hurt:
+			if (IsAnimationFinished()) {
+				state = State.Idle;
+			}
+			break;
 		}
 
 	}
@@ -147,6 +152,10 @@ public class SlimeEnemy : WorldObject {
 	}
 
 	public override bool ReceiveDamage(int damage) {
+		if (state == State.Hurt) {
+			return false;
+		}
+
 		jumpTrigger = false;
 		jumping = false;
 		state = State.Hurt;
@@ -155,7 +164,7 @@ public class SlimeEnemy : WorldObject {
 		if (currentHitPoints <= 0) {
 			Die();
 		}
-		return true;
+		return true;	
 	}
 
 	private void Die() {
