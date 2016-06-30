47b6cdddb4a0bbaa76c7e7671298946b80197577
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-07-01 05:07:21 +0900

    Potion Sound Effect.

--- Assets/Script/Potion.cs
+++ Assets/Script/Potion.cs
@@ -9,12 +9,14 @@ using System.Collections;
 
 public class Potion : MonoBehaviour {
 
+	public AudioClip potionSound;
 	public bool killTrigger = false;
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
 			Player player = other.gameObject.GetComponent<Player>();
 			player.Health += 2;
+			AudioManager.instance.PlaySound(potionSound);
 			Destroy(gameObject.transform.parent.gameObject);
 		}
 	}
