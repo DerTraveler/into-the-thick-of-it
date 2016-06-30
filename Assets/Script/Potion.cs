a6e8a482ca4536b620eeb7f1e3bf67b499ed0b56
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-07-01 04:51:45 +0900

    Heal Potion added.

---
+++ Assets/Script/Potion.cs
@@ -0,0 +1,27 @@
+﻿/* Copyright (c) 2016 Kevin Fischer
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+using UnityEngine;
+using System.Collections;
+
+public class Potion : MonoBehaviour {
+
+	public bool killTrigger = false;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Player player = other.gameObject.GetComponent<Player>();
+			player.Health += 2;
+			Destroy(gameObject.transform.parent.gameObject);
+		}
+	}
+
+	void Update() {
+		if (killTrigger) {
+			Destroy(gameObject.transform.parent.gameObject);
+		}
+	}
+}
--- CREDITS.md
+++ CREDITS.md
@@ -3,6 +3,7 @@ Credits
 
 ### Graphic Resources
 * Player Sprite by b_0 on [OpenGameArt](http://opengameart.org/content/saxon-swordsman) released under [GPLv2](http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+* Potion Sprite by yafarida on [OpenGameArt](http://opengameart.org/content/pixel-potion-set-16x16)
 
 ## Background Music:
 * [Field04](http://maoudamashii.jokersounds.com/archives/game_maoudamashii_4_field04.html) by Maoudamashii (JP)
