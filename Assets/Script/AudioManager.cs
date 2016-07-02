98699254149aef8889c0fa811c7bab4a8f674c35
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-07-02 23:55:56 +0900

    Add AudioManager.

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿/* Copyright (c) 2016 Kevin Fischer
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioSource sfxSource;
+	public AudioSource musicSource;
+
+	public static AudioManager instance = null;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void PlaySound(AudioClip clip) {
+		sfxSource.clip = clip;
+		sfxSource.Play();
+	}
+
+}
