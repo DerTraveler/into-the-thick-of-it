efd6c3ece787cba41c9bd2109b56fbf9f0891599
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-07-07 18:43:28 +0900

    Add State base class.

---
+++ Assets/Script/States/State.cs
@@ -0,0 +1,19 @@
+﻿/* Copyright (c) 2016 Kevin Fischer
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+using UnityEngine;
+
+public abstract class State<T> {
+
+	public virtual void Entry(T subject) { }
+
+	public virtual void Exit(T subject) { }
+
+	public abstract State<T> HandleInput(T subject);
+	
+	public abstract void Update(T subject);
+
+}
