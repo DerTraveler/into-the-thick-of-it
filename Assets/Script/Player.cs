3e85d4453907821ea05929fd55e19b4183191d84
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-07-07 19:34:07 +0900

    Switch from old system to State system (only Idle and Walking)

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,8 +5,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections.Generic;
 using States;
 
 public class Player : Actor {
@@ -32,21 +30,7 @@ public class Player : Actor {
 		public static PlayerWalking WALKING = new PlayerWalking();
 	}
 
-	public enum State {
-		Idle,
-		Walking,
-		Attacking,
-		Hurt,
-		Dead
-	}
-
-	private State state = State.Idle;
-	private string currentAnimation = "IdleDown";
-
-	private bool directionPressed = false;
-
-	private bool attackTrigger = false;
-	private bool hurtTrigger = false;
+	private State<Player> _state = States.IDLE;
 
 	void Start () {
 		Animator.SetFloat("Speed", speed);
@@ -55,101 +39,19 @@ public class Player : Actor {
 
 	// Update is called once per frame
 	void Update () {
-		ReadInput();
+		State<Player> newState = _state.HandleInput(this);
 
-		UpdateState();
-
-		PlayAnimation();
-
-		UpdateTransform();
-	}
-
-	private void ReadInput() {
-		MoveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-		directionPressed = MoveDirection.sqrMagnitude > float.Epsilon;
-
-		attackTrigger = Input.GetButtonDown("Attack");
-
-		if (Input.GetButtonDown("Cancel")) {
-			Application.Quit();
+		if (newState != null) {
+			_state.Exit(this);
+			newState.Entry(this);
+			_state = newState;
 		}
+
+		_state.Update(this);
 	}
 
-	private void UpdateState() {
-		if (_health <= 0 && state != State.Dead) {
-			GetComponent<SpriteRenderer>().enabled = false;
-			GetComponent<Collider2D>().enabled = false;
-			gameOverText.enabled = true;
-			state = State.Dead;
-		} else {
-			switch(state) {
-			case State.Idle:
-				if (directionPressed) {
-					state = State.Walking;
-				}
-				break;
-			case State.Walking:
-				if (!directionPressed) {
-					state = State.Idle;
-				}
-				break;
-			case State.Attacking:
-				ResumeAfterAnimation();
-				break;
-			case State.Hurt:
-				if (hurtTrigger) {
-					hurtTrigger = false;
-				} else {
-					ResumeAfterAnimation();
-				}
-				break;
-			case State.Dead:
-				if (attackTrigger) {
-					int scene = SceneManager.GetActiveScene().buildIndex;
-					SceneManager.LoadScene(scene, LoadSceneMode.Single);
-				}
-				break;
-			}
-
-			if (attackTrigger && (state == State.Idle || state == State.Walking)) {
-				state = State.Attacking;
-			}
-		}
-	}
-
-	private void ResumeAfterAnimation() {
-		if (IsAnimationFinished()) {
-			// Return to walk or idle after end of animation
-			state = directionPressed ? State.Walking : State.Idle;
-		}
-	}
-
-	private void PlayAnimation() {
-		if (state != State.Dead) {
-			string nextAnimation = DirectedAnimationName(state.ToString());
-
-			if (nextAnimation != currentAnimation) {
-				currentAnimation = nextAnimation;
-				Animator.Play(currentAnimation);
-			}
-		}
-	}
-
-	private void UpdateTransform() {
-		if (state == State.Walking) {
-			Vector3 newPos = transform.position + (Vector3)(MoveDirection * speed * Time.deltaTime);
-			transform.position = newPos;
-		}
-	}
 
 	public override bool ReceiveDamage(int damage) {
-		if (state == State.Hurt) {
-			return false;
-		}
-
-		state = State.Hurt;
-		hurtTrigger = true;
 
 		Health -= damage;
 
