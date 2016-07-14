c0e99e529c79db05b4671a8d04da4016723804e6
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-07-14 18:09:41 +0900

    Initial Commit Editor Window

---
+++ Assets/Script/Editor/StateMachineEditorWindow.cs
@@ -0,0 +1,40 @@
+﻿/* Copyright (c) 2016 Kevin Fischer
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace StateMachine.Editor {
+
+	public class StateMachineEditorWindow : EditorWindow {
+
+		private static StateMachineEditorWindow _window;
+		
+		[MenuItem("Window/State Machine Editor")]
+		public static void ShowWindow() {
+			_window = GetWindow<StateMachineEditorWindow>();
+			_window.titleContent = new GUIContent("State Machine Editor");
+		}
+
+		[UnityEditor.Callbacks.OnOpenAsset(1)]
+		public static bool OpenStateMachine(int instanceID, int line) {
+			if (Selection.activeObject as StateMachine != null) {
+				ShowWindow();
+
+				return true;
+			}
+			return false;
+		}
+
+		void OnGUI () {
+			
+		}
+
+
+	}
+
+}
